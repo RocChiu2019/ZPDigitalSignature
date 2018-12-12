0a674d552889da8d2d0630aa9242d24c9848773f
赵鹏 <you@example.com>
2018-12-12 16:40:29 +0800

    创建项目。

---
+++ 数字签名原理/数字签名原理/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  数字签名原理
+//
+//  Created by 赵鹏 on 2018/12/11.
+//  Copyright © 2018 赵鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 数字签名原理/数字签名原理/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  数字签名原理
+//
+//  Created by 赵鹏 on 2018/12/11.
+//  Copyright © 2018 赵鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
